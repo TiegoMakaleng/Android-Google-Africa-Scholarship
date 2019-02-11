beec8d99b423b028251a54c2674d7ce487df6361
Tiego Makaleng <tiego.makaleng@gmail.com>
2019-02-11 02:40:06 +0200

    Exercises and Project Sunshine

---
+++ Exercises/Lesson10-Hydration-Reminder/T10.01-Exercise-IntentServices/app/src/main/java/com/example/android/background/sync/ReminderTasks.java
@@ -0,0 +1,21 @@
+package com.example.android.background.sync;
+
+// TODO (1) Create a class called ReminderTasks
+public class ReminderTasks {
+// TODO (2) Create a public static constant String called ACTION_INCREMENT_WATER_COUNT
+   public static final String ACTION_INCREMENT_WATER_COUNT = "increment-water-count";
+// TODO (6) Create a public static void method called executeTask
+   public static void executeTask(Context context, String action) {
+// TODO (7) Add a Context called context and String parameter called action to the parameter list
+// TODO (8) If the action equals ACTION_INCREMENT_WATER_COUNT, call this class's incrementWaterCount
+     if (ACTION_INCREMENT_WATER_COUNT.equals(action) {
+		 incrementWaterCount(context);
+	 }
+   }
+// TODO (3) Create a private static void method called incrementWaterCount
+   private static void incrementWaterCount(Context context) {
+// TODO (4) Add a Context called context to the argument list
+// TODO (5) From incrementWaterCount, call the PreferenceUtility method that will ultimately update the water count
+    PreferenceUtilities.incrementWaterCount(context);
+   }
+}  
